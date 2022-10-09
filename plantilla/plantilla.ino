34d3ee2b6da1f12bb0ac6f0e311ca120531d6dbe
fraromesc <fraromesc@gmail.com>
2022-10-09 14:01:04 +0200

    intento practica 2

---
+++ plantilla/plantilla.ino
@@ -0,0 +1,131 @@
+//motor 1 in1 in2 ena
+#define IN1 4
+#define IN2 5
+#define ENA 3
+
+//motor 2 in3 in4 enb
+#define IN3 7
+#define IN4 8
+#define ENB 6
+
+#define stopA 0
+#define stopB 0
+
+#define trigDer 52
+#define echoDer 50
+
+#define trigIzq 24
+#define derIzq 22
+
+int distanciaUS(int, int)
+void avanza(int)
+void retrocede(int)
+void para()
+void giroIzq()
+void giroDer()
+void pivotIzq()
+void pivotDer()
+void setup(){
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (ENA, OUTPUT);
+  pinMode (ENB, OUTPUT);
+  Serial.begin(9600); 
+  //establecer direcciones
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  //inicar los motores quietos
+  analogWrite(ENA, 80); 
+  analogWrite(ENB, 80);
+}
+void loop() {
+  
+   
+}
+void avanza(int vel)
+{
+  int velA = vel;
+  int velB = (85/80)*vel; //Manteniendo proporción para rectitud 
+  
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  
+  analogWrite(ENA, velA);
+  analogWrite(ENB, velB); 
+}
+void retrocede(int vel)
+{
+  int velA = vel;
+  int velB = (85/80)*vel; //Manteniendo proporción para rectitud 
+  
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  
+  analogWrite(ENA, velA);
+  analogWrite(ENB, velB); 
+}
+void para()
+{
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0); 
+}
+void giroIzq()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH); 
+  analogWrite(ENB, 85); 
+  analogWrite(ENA, 10); 
+}
+void giroDer()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH); 
+  analogWrite(ENB, 10); 
+  analogWrite(ENA, 80); 
+}
+
+void pivotIzq()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 80); 
+  analogWrite(ENB, 85);
+}
+
+void pivotDer()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 80); 
+  analogWrite(ENB, 85); 
+}
+int distanciaUS(int TriggerPin, int EchoPin) {
+   long duration, distanceCm;
+   
+   digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+   delayMicroseconds(4);
+   digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+   delayMicroseconds(10);
+   digitalWrite(TriggerPin, LOW);
+   
+   duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+   
+   distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+   return distanceCm;
+}
